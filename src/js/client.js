2edf58b1b7c5b2a8cf4f1a78e6928a3d7ee84a8a
wistcc <wistcc@hotmail.com>
2016-12-30 11:09:02 -0400

    First commit

---
+++ src/js/client.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import werewolfBrain from 'werewolf-brain';
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var n = getRandom(1, 50);
+var m = getRandom(0, 1);
+var x = getRandom(0, 4);
+var mode = m ? 'Chaos' : 'Balanced';
+
+var t = {
+  0: 'Basic',
+  1: 'Novice',
+  2: 'Amateur',
+  3: 'Wolfpack',
+  4: 'Competent'
+};
+
+class Main extends React.Component {
+  render() {
+    let game = {};
+    let htmlText = '';
+
+    if (m) game = werewolfBrain.getChaosGameFromTemplate(n, t[x]);
+    else game = werewolfBrain.getGameFromTemplate(n, t[x]);
+
+    game.deck = Object.keys(game.deck).map(function (card, index) {
+      return game.deck[card] + " " + card;
+    });
+
+    for (var i = 0; i < game.deck.length; i++) {
+      htmlText += game.deck[i] + " *** \n";
+    }
+
+    return (
+      <div>
+        <h1>Werewolf Moderator</h1>
+        <p>Deck: {htmlText}</p>
+        <p>Weight: {game.weight}</p>
+        <p>Players: {game.players}</p>
+      </div>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Main />, app);
